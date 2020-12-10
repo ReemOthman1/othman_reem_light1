2f3339e0a9db6921024e24cade6e919d9ed74de0
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-12-10 13:00:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ while True:
         light.set_all(light.rgb(0, 0, 255))
     elif:
         if input.light_level() > 7:
-            light.set_all(light.rgb(0, 0, 255))
+            light.set_all(light.rgb(255, 255, 0))
     else:
          light.clear()   
