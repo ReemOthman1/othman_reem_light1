2f24070a24a9d44a22fc221b689232a0666c8b6b
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-12-10 12:59:37 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 while True:
     print("light level:"+ input.light_level())
     if input.light_level() < 7: 
-        light.set_all(light.rgb(255, 255, 255))
+        light.set_all(light.rgb(0, 0, 255))
+    elif:
+        if input.light_level() > 7:
+            light.set_all(light.rgb(0, 0, 255))
     else:
          light.clear()   
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("light level:" + input.lightLevel())
     if (input.lightLevel() < 7) {
-        light.setAll(light.rgb(255, 255, 255))
+        light.setAll(light.rgb(0, 0, 255))
     } else {
         light.clear()
     }
