486e8cb7757b052bc327328f811573e6d0a3a947
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-12-11 01:24:44 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("light level:"+ input.light_level())
-    if input.light_level() < 100: 
+    if input.light_level() < 9: 
         light.set_all(light.rgb(0, 0, 255))
     elif input.light_level() > 500:
             light.set_all(light.rgb(255, 255, 0))
